f220de5b875d113bbb116d8023cea04f1c5f5bd2
eunhyung <ooooo5h91@gmail.com>
2022-10-24 18:44:18 +0900

    [MODIFY] 팀원 코드 전체 삭제 및 TYPEORM -> MYSQL2 연결로 변경

---
+++ models/userDao.js
@@ -0,0 +1,3 @@
+const pool = require("./mysql")
+
+// await pool.query
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const usersRouter = require("./user");
-const measurementRouter = require("./measurement");
 
-router.use("/users", usersRouter);
-router.use("/measurement-data", measurementRouter);
-router.use("", measurementRouter);
+
 
 module.exports = router;
